c7c259fd61ffa896a4db2f8b646f3889213739c3
Patryk Wałach <patryk_walach@outlook.com>
2019-09-18 21:26:07 +0200

    feat(plugin): sync commits made to store

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,13 +1,35 @@
-import { Module } from './module'
+import { Module, SetupReturnType } from './module'
 import { Store } from 'vuex'
+import { mutable } from './apis/mutation'
 
-export const Plugin = (modules: Module<any>[]) => (store: Store<any>) =>
-  modules.forEach(({ name, rawModule, subscribe }) => {
-    store.registerModule(name, rawModule)
+export const Plugin = (modules: Module<SetupReturnType>[]) => (
+  store: Store<any>,
+) =>
+  modules.forEach(
+    ({ name, state, rawModule, subscribe, _mutations, options }) => {
+      store.registerModule(name, rawModule)
 
-    // store.subscribe()
+      store.subscribe(({ type, payload }) => {
+        let mutationName: string
+        if (options.namespaced) {
+          const splitName = type.split('/')
 
-    subscribe((mutationName, state, payload) => {
-      store.commit(mutationName, payload)
-    })
-  })
+          if (splitName.length === 2) {
+            const [moduleName, _mutationName] = splitName
+            if (moduleName === name) {
+              mutationName = _mutationName
+            }
+          }
+        } else {
+          mutationName = type
+        }
+        if (_mutations.hasOwnProperty(mutationName)) {
+          _mutations[mutationName](mutable(state), payload)
+        }
+      })
+
+      subscribe((mutationName, state, payload) => {
+        store.commit(mutationName, payload)
+      })
+    },
+  )
--- tests/unit/Plugin.spec.ts
+++ tests/unit/Plugin.spec.ts
@@ -26,7 +26,7 @@ describe('CompositionApi.Plugin', () => {
     expect(store.state.main.data).toStrictEqual(test)
   })
 
-  it('keeps Vuex in sync with the modules', () => {
+  it('dispatches mutations inside Vuex', () => {
     const test = 'null'
 
     const Main = new CompositionApi.Module({
@@ -58,5 +58,39 @@ describe('CompositionApi.Plugin', () => {
     Main.mutations.CHANGE_DATA(test)
 
     expect(store.state.main.data).toStrictEqual(test)
+  })
+
+  it('dispatches mutations from Vuex', () => {
+    const test = 'null'
+
+    const Main = new CompositionApi.Module({
+      name: 'main',
+      namespaced: true,
+      setup({ state, mutation }) {
+        const data = state('')
+        return {
+          state: {
+            data,
+          },
+          mutations: {
+            CHANGE_DATA: mutation(
+              'CHANGE_DATA',
+              { data },
+              (state, payload: string) => {
+                state.data = payload
+              },
+            ),
+          },
+        }
+      },
+    })
+
+    const store = new CompositionApi.Store({
+      plugins: [CompositionApi.Plugin([Main])],
+    })
+
+    store.commit('main/CHANGE_DATA', test)
+
+    expect(Main.state.data.value).toStrictEqual(test)
   })
 })
