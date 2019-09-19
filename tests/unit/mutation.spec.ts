af14d83903157ec530aa2e0d15bd2a38ea52b7a3
Patryk Wałach <patryk_walach@outlook.com>
2019-09-19 18:19:26 +0200

    fix(mutation.spec): add tests for mutable

--- tests/unit/mutation.spec.ts
+++ tests/unit/mutation.spec.ts
@@ -1,27 +1,17 @@
-import CompositionApi, { state } from '../../src'
+import CompositionApi, { state, mutable } from '../../src'
 
 import { createLocalVue } from '@vue/test-utils'
 
 const localVue = createLocalVue()
 localVue.use(CompositionApi)
 
-describe('mutation()', () => {
-  new CompositionApi.Module({
-    name: '',
-    setup({ mutation }) {
-      it('makes state mutable', () => {
-        const x = state('')
-        const CHANGE_X = mutation('CHANGE_X', { x }, (state, newX: string) => {
-          state.x = newX
-        })
+describe('mutable()', () => {
+  it('makes state mutable', () => {
+    const x = state('')
 
-        const test = 'null'
-        CHANGE_X(test)
+    const test = 'null'
+    mutable({ x }).x = test
 
-        expect(x.value).toStrictEqual(test)
-      })
-
-      return {}
-    },
+    expect(x.value).toStrictEqual(test)
   })
 })
