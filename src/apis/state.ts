c76a117d6c36720544d258f0e6a58b19aaa7802e
Patryk Wałach <patryk_walach@outlook.com>
2019-11-20 20:28:05 +0100

    feat(state): replace it with reactive

--- src/apis/state.ts
+++ src/apis/state.ts
@@ -1,31 +1,3 @@
-import { Ref, ref } from '@vue/composition-api'
-import { assert } from '../utils'
-
-export type State<T> = Ref<T> & { _replace(value: T): void }
-
-export const state = <T>(value: T): State<T> => {
-  const _value: Ref<T> = ref(value)
-
-  return new Proxy(_value, {
-    get: (obj, prop) => {
-      if (prop === '_replace') {
-        return (newValue: T) => {
-          _value.value = newValue
-        }
-      }
-
-      assert(
-        prop === 'value',
-        `prop ${String(prop)} doesn't exist on type State`,
-      )
-
-      return obj.value
-    },
-    set: () => {
-      if (process.env.NODE_ENV !== 'production') {
-        assert(false, 'use state._replace() to explicit replace state.')
-      }
-      return true
-    },
-  }) as State<T>
-}
+import { UnwrapRef } from '@vue/composition-api/dist/reactivity'
+export type State<T> = UnwrapRef<T>
+export { reactive as state } from '@vue/composition-api'
