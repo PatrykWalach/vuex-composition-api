2624986bdf30a16163f6e90daa1fb4b8de3f5a83
Patryk Wałach <patryk_walach@outlook.com>
2019-11-20 20:28:45 +0100

    feat: remove mutable

--- src/apis/mutation.ts
+++ src/apis/mutation.ts
@@ -1,78 +1,18 @@
-import { RawModule } from '../module'
 import { State } from './state'
-import { assert } from '../utils'
-
-export type inferState<S extends Record<string, State<any>> | undefined> = {
-  [K in keyof S]: S[K] extends State<infer T> ? T : unknown
-}
-
-export const mutable = <S extends Record<string, State<any>>>(
-  state: S,
-): inferState<S> =>
-  new Proxy(state, {
-    get: (state, prop: keyof typeof state) => {
-      return state[prop] && state[prop].value
-    },
-    set: (state, prop: keyof typeof state, _value) => {
-      state[prop]._replace(_value)
-      return true
-    },
-  }) as inferState<S>
-
-export type BoundMutation = {
-  <S extends Record<string, State<any>>>(
-    name: string,
-    state: S,
-    fn: (state: inferState<S>, payload: void) => void,
-  ): (payload: void) => void
-
-  <S extends Record<string, State<any>>, O>(
-    name: string,
-    state: S,
-    fn: (state: inferState<S>, payload: O) => void,
-  ): (payload: O) => void
-}
 
 export type Mutation = {
   <S extends Record<string, State<any>>>(
-    module: RawModule,
-    name: string,
     state: S,
-    fn: (state: inferState<S>, payload: void) => void,
-  ): (payload: void) => void
+    fn: (state: S, payload: void) => any,
+  ): (state: S, payload: void) => any
 
   <S extends Record<string, State<any>>, O>(
-    module: RawModule,
-    name: string,
     state: S,
-    fn: (state: inferState<S>, payload: O) => void,
-  ): (payload: O) => void
+    fn: (state: S, payload: O) => any,
+  ): (state: S, payload: O) => any
 }
 
-export const mutation: Mutation = <S extends Record<string, State<any>>, O>(
-  module: RawModule,
-  name: string,
+export const mutation: Mutation = <S extends State<any>, O>(
   state: S,
-  fn: (state: inferState<S>, payload?: O) => void,
-) => {
-  assert(
-    !module._mutations.hasOwnProperty(name),
-    'mutation names have to be unique',
-  )
-
-  module._mutations[name] = fn
-
-  return (payload?: O) => {
-    if (module._store) {
-      const type = module.name.length ? [...module.name, name].join('/') : name
-
-      module._store({ payload, type }, state)
-    } else {
-      fn(mutable(state), payload)
-    }
-
-    module._subscribers.forEach(callback =>
-      callback({ payload, type: name }, state),
-    )
-  }
-}
+  fn: (state: S, payload?: O) => any,
+) => fn
